f4b0671773be83a5b1e33a006f166a08f8e684cd
Giorgio Ventura <giorgio.ventura@tismart.com>
2020-06-13 08:47:10 -0500

    updating hotel gatsby

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,33 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
 
-// You can delete this file if you're not using it
+exports.createPages = async ({ actions, graphql, reporter }) => {
+    const resultado = await graphql(`
+        query {
+            allDatoCmsHabitacion {
+                nodes {
+                    slug
+                }
+            }
+        }
+    `);
+
+    // console.log(resultado.data.allDatoCmsHabitacion.nodes);
+    if(resultado.errors) {
+        reporter.panic('No hubo resultados ', resultado.errors);
+    }
+
+    // Si hay paginas, crear los archivos
+    const habitaciones = resultado.data.allDatoCmsHabitacion.nodes;
+
+    habitaciones.forEach(habitacion => {
+        actions.createPage({
+            path: habitacion.slug,
+            component: require.resolve('./src/components/habitaciones.js'),
+            context: {
+                slug: habitacion.slug
+            }
+        })
+    })
+
+
+    
+}
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,51 +1,69 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 import React from "react"
-import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
-
+import Helmet from "react-helmet"
+import { Global, css } from "@emotion/core"
 import Header from "./header"
-import "./layout.css"
+import Footer from "./footer"
+import useSeo from "../hooks/use-seo"
 
-const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
-
+const Layout = props => {
+  const seo = useSeo()
+  const {siteName,fallbackSeo:{description,title}} = seo
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
-      </div>
+      <Global
+        styles={css`
+          html {
+            font-size: 62.5%;
+            box-sizing: border-box;
+          }
+          *,
+          *:before,
+          *:after {
+            box-sizing: inherit;
+          }
+          body {
+            font-size: 18px;
+            font-size: 1.8rem;
+            line-height: 1.5;
+            font-family: "PT Sans", sans-serif;
+          }
+          h1,
+          h2,
+          h3 {
+            margin: 0;
+            line-height: 1.5;
+          }
+          h1,
+          h2 {
+            font-family: "Roboto", serif;
+          }
+          h3 {
+            font-family: "PT Sans", sans-serif;
+          }
+          ul {
+            list-style: none;
+            margin: 0;
+            padding: 0;
+          }
+        `}
+      />
+      <Helmet>
+        <title>{title}</title>
+        <meta name="description" content={description} />
+        <link
+          href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&family=Roboto:wght@400;700&display=swap"
+          rel="stylesheet"
+        />
+      </Helmet>
+      <Header />
+      {props.children}
+      <Footer  title={title}/>
     </>
   )
-}
-
-Layout.propTypes = {
-  children: PropTypes.node.isRequired,
 }
 
 export default Layout
